bcec2200330d7c474e97de0b3f6121286b9ea8a5
ShenJieSuzhou <745689122@qq.com>
2023-07-29 21:12:09 +0800

    角色Idle,Walk 动画

--- ROTD/Source/ROTD/ROTDGameMode.cpp
+++ ROTD/Source/ROTD/ROTDGameMode.cpp
@@ -8,7 +8,8 @@ AROTDGameMode::AROTDGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	// static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/ROTD/Blueprint/MainCharacter_BP"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 }
