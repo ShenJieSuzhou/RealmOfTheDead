878f5fe1daabdfab17ad1e65e0726249941feeff
ShenJieSuzhou <745689122@qq.com>
2023-07-30 22:41:52 +0800

    新增武器大类

---
+++ ROTD/Source/ROTD/Weapons/WeaponBase.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WeaponBase.h"
+
+// Sets default values
+AWeaponBase::AWeaponBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set Gun
+	FP_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FP_Gun"));
+	FP_Gun->SetOnlyOwnerSee(false);
+	FP_Gun->bCastDynamicShadow = false;
+	FP_Gun->CastShadow = false;
+	FP_Gun->SetupAttachment(RootComponent);
+
+	// Set MuzzleLocation
+	FP_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("MuzzleLocation"));
+	FP_MuzzleLocation->SetupAttachment(FP_Gun);
+	
+	// Set PointLight
+	FP_PointLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLight"));
+	FP_PointLight->SetIntensity(0);
+	FP_PointLight->SetupAttachment(FP_Gun);
+
+	// SetPlane
+	FP_FlashPlane = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GunFlash"));
+	FP_FlashPlane->SetVisibility(false);
+	FP_FlashPlane->SetupAttachment(FP_Gun);
+}
+
+// Called when the game starts or when spawned
+void AWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWeaponBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ ROTD/Source/ROTD/Weapons/WeaponBase.h
@@ -0,0 +1,103 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/EngineTypes.h"
+#include "Sound/SoundBase.h"
+#include "Components/PointLightComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "WeaponBase.generated.h"
+
+class USkeletalMeshComponent;
+class USceneComponent;
+class UAnimMontage;
+class USoundBase;
+
+UENUM()
+enum class EWeapon : uint8
+{	
+	EW_None,
+	EW_Knife,
+	EW_Pisto,
+	EW_Rifle
+};
+
+UCLASS()
+class ROTD_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	/** Gun mesh: 1st person view (seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USkeletalMeshComponent* FP_Gun;
+
+	/** Location on gun mesh where projectiles should spawn. */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	USceneComponent* FP_MuzzleLocation;
+	
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	UPointLightComponent* FP_PointLight;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+	UStaticMeshComponent* FP_FlashPlane;
+
+	// Projectile class to spawn
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	EWeapon WeaponType;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	USoundBase* FireSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	USoundBase* DryFireSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	USoundBase* ReloadDropSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	USoundBase* ReloadInsertSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	USoundBase* ReloadLoadSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	UAnimMontage* FireAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	FString GunName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	float ReloadTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	float BulletSpread;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	int MaxAmmoCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	float Damage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	bool CanScope;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	bool CanAutomatic;
+
+public:	
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
