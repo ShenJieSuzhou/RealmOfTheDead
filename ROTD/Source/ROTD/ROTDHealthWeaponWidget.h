35a2b85d0c02bb00a301f0a40cdf28b2fea72bdc
Code by a Cat <745689122@qq.com>
2023-09-12 18:02:26 +0800

    新增武器血量显示界面控制类

---
+++ ROTD/Source/ROTD/ROTDHealthWeaponWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ROTDHealthWeaponWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROTD_API UROTDHealthWeaponWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
